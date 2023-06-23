6270a5b3b646e302e42809cfa32726cb21d31842
Dideldidu <ewfccfcvw@ievmi.com>
2023-06-23 16:51:27 +0200

    This commit will add slmmaintenancemonitorController and default feature

---
+++ src/main/java/com/example/slmmaintenancemonitor/slmmaintenancemonitorController.java
@@ -0,0 +1,9 @@
+package com.example.slmmaintenancemonitor;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class slmmaintenancemonitorController {
+
+    String defaultmessage = "Everything works as expected";
+}
