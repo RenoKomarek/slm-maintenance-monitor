0fed0ba5f45e29391990e530eecf4c54da36b46e
Mert Sali <mert.sali@outlook.com>
2023-06-23 17:12:58 +0200

    This commit will add message feature

--- src/main/java/com/example/slmmaintenancemonitor/slmmaintenancemonitorController.java
+++ src/main/java/com/example/slmmaintenancemonitor/slmmaintenancemonitorController.java
@@ -1,9 +1,16 @@
 package com.example.slmmaintenancemonitor;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class slmmaintenancemonitorController {
 
     String defaultmessage = "Everything works as expected";
+
+    String m = defaultmessage;
+
+    @RequestMapping("/api/message")
+    public String message(){ return m; }
+
 }
